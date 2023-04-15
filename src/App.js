b7985b308298339d3917f949b548e8abc45fa663
Ivan Lenkavets <47rex47@mail.ru>
2023-04-15 18:49:51 +0300

    feat: add navbar, home, list

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import{
   Routes,
   Route,
 }from "react-router-dom";
+import Home from "./pages/home/home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/hotel";
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/hotels" element = {<List/>}/>
+        <Route path="/hotels/:id" element = {<Hotel/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
